3905904da84bbf422f46f0b9b16f5f512a5b9c78
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-01-22 10:13:42 +0300

    added contact form validation and processing

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-footer',
@@ -8,14 +10,35 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class FooterComponent implements OnInit {
   myForm: FormGroup;
+  submitted = false;
+  message = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit() {
     this.myForm = this.formBuilder.group({
-
+      fullname: ['', Validators.required],
+      email: ['', Validators.required],
+      message: ['', Validators.required]
     });
-
   }
 
+  get f() {
+    return this.myForm.controls;
+  }
+
+  submit() {
+    this.submitted = true;
+
+    if (this.myForm.invalid) {
+       return;
+     }
+    this.http.post('https://formspree.io/xzbeogkk', this.myForm.value).subscribe(
+      res => {
+        if (res.hasOwnProperty('next') === true) {
+          this.message = true;
+        }
+      }
+    );
+  }
 }
